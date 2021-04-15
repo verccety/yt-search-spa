a1d62cc900e4dcd4d4c6afab744a1a8330d9c513
Pavel <verccety1@gmail.com>
2021-04-15 17:54:13 +0700

    Add ErrorBoundary

---
+++ src/Components/ErrorBoundary/ErrorBoundary.component.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { ErrorImageOverlay, ErrorImageContainer, ErrorImageText } from './ErrorBoundary.styles';
+class ErrorBoundary extends Component {
+  state = {
+    hasErrored: false,
+  };
+  // thrown error if any thrown in children
+  static getDerivedStateFromError(error) {
+    return {
+      hasErrored: true,
+    };
+  }
+
+  componentDidCatch(error, info) {}
+  render() {
+    return (
+      <div>
+        {this.state.hasErrored ? (
+          <ErrorImageOverlay>
+            <ErrorImageContainer imageUrl={'https://i.imgur.com/QIxIKBH.png'} />
+            <ErrorImageText>Что-то пошло не так..</ErrorImageText>
+          </ErrorImageOverlay>
+        ) : (
+          this.props.children
+        )}
+      </div>
+    );
+  }
+}
+
+export default ErrorBoundary;
